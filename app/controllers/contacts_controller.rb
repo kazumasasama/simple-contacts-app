0c7c80200965a34b5e332aa8d7fe716c06a1b29d
Peter Jang <peter.x.jang@gmail.com>
2022-02-04 14:45:29 -0600

    Add contacts RESTful CRUD actions

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,46 @@
+class ContactsController < ApplicationController
+  def index
+    contacts = Contact.all
+    render json: contacts
+  end
+
+  def create
+    contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      image: params[:image],
+    )
+    if contact.save
+      render json: contact
+    else
+      render json: { errors: contact.errors.full_messages }, status: 422
+    end
+  end
+
+  def show
+    contact = Contact.find_by(id: params[:id])
+    render json: contact
+  end
+
+  def update
+    contact = Contact.find_by(id: params[:id])
+    contact.first_name = params[:first_name] || contact.first_name
+    contact.last_name = params[:last_name] || contact.last_name
+    contact.email = params[:email] || contact.email
+    contact.phone_number = params[:phone_number] || contact.phone_number
+    contact.image = params[:image] || contact.image
+    if contact.save
+      render json: contact
+    else
+      render json: { errors: contact.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    contact = Contact.find_by(id: params[:id])
+    contact.destroy
+    render json: { message: "Contact destroyed successfully!" }
+  end
+end
